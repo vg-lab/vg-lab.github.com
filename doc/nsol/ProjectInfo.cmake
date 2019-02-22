0c74582920a932463239f6bca753196479412d57
Pablo Toharia <pablo.toharia@urjc.es>
2019-02-22 14:26:23 +0100

    Buildbot updated doc for nsol

--- doc/nsol/ProjectInfo.cmake
+++ doc/nsol/ProjectInfo.cmake
@@ -1,6 +1,6 @@
 set(NSOL_MATURITY "EP")
 set(NSOL_DESCRIPTION "Nsol - Neuroscience Objects Library")
-set(NSOL_GIT_ROOT_URL "https://gitlab-ci-token:HXpGfJVGZ5dDkxA_GRy-@gitlab.gmrv.es/nsviz/nsol.git")
+set(NSOL_GIT_ROOT_URL "https://gitlab-ci-token:RtnkSLZfNW8_VAH8R4gy@gitlab.gmrv.es/nsviz/nsol.git")
 set(NSOL_PACKAGE_URL "")
 set(NSOL_CI_URL "")
 set(NSOL_CI_PNG "")
