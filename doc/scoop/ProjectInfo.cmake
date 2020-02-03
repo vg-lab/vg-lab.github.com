8bcd1237f6fb56d33e1dc5470121dd4f6bf4c7d6
Pablo Toharia <pablo.toharia@upm.es>
2020-02-03 11:07:50 +0100

    Buildbot updated doc for scoop

--- doc/scoop/ProjectInfo.cmake
+++ doc/scoop/ProjectInfo.cmake
@@ -1,6 +1,6 @@
 set(SCOOP_MATURITY "EP")
 set(SCOOP_DESCRIPTION "Scoop - Simple ColoR pAleTtes and mappERs")
-set(SCOOP_GIT_ROOT_URL "https://gitlab-ci-token:UuYnrrFpUywzgLD2LjEH@gitlab.gmrv.es/gmrv/scoop.git")
+set(SCOOP_GIT_ROOT_URL "https://gitlab-ci-token:UvK74ABtfz-HqNNatsve@gitlab.gmrv.es/gmrv/scoop.git")
 set(SCOOP_PACKAGE_URL "")
 set(SCOOP_CI_URL "")
 set(SCOOP_CI_PNG "")
