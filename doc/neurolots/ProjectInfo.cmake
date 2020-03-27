9e0055f69e828a7339516224be0cf51784dac704
Pablo Toharia <pablo.toharia@upm.es>
2020-03-27 12:15:16 +0100

    Buildbot updated doc for neurolots

--- doc/neurolots/ProjectInfo.cmake
+++ doc/neurolots/ProjectInfo.cmake
@@ -1,6 +1,6 @@
 set(NEUROLOTS_MATURITY "EP")
 set(NEUROLOTS_DESCRIPTION "NeuroLOTs")
-set(NEUROLOTS_GIT_ROOT_URL "https://gitlab-ci-token:kjvANyRMKMA4227yDtyh@gitlab.gmrv.es/nsviz/neurolots.git")
+set(NEUROLOTS_GIT_ROOT_URL "https://gitlab-ci-token:rvGF-oiLD7AysTHsNyeT@gitlab.gmrv.es/nsviz/neurolots.git")
 set(NEUROLOTS_PACKAGE_URL "")
 set(NEUROLOTS_CI_URL "")
 set(NEUROLOTS_CI_PNG "")
