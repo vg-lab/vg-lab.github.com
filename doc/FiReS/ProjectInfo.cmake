16bdd4dd7f73dbb8e002a66989bb1f081e2bb0bd
Pablo Toharia <pablo.toharia@upm.es>
2021-04-06 13:37:32 +0200

    Buildbot updated doc for FiReS

--- doc/FiReS/ProjectInfo.cmake
+++ doc/FiReS/ProjectInfo.cmake
@@ -1,6 +1,6 @@
 set(FIRES_MATURITY "EP")
 set(FIRES_DESCRIPTION "")
-set(FIRES_GIT_ROOT_URL "https://gitlab-ci-token:gg9PPuUgx_kbPvyD7DQC@gitlab.vglab.es/retrieval/FiReS.git")
+set(FIRES_GIT_ROOT_URL "https://gitlab-ci-token:j7Y4EzPszMAb2X7hstP9@gitlab.vg-lab.es/retrieval/FiReS.git")
 set(FIRES_PACKAGE_URL "")
 set(FIRES_CI_URL "")
 set(FIRES_CI_PNG "")
